19660ab5238f2a644f3b6e60fdfe39cd73a02eae
김석진 <7271kim@naver.com>
2021-04-05 23:37:57 +0900

    [2021.04.05] Upload 텝 추가

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+ export default class UploadProductPage extends React.Component {
+    render(){
+        return (
+            <div>
+                안녕하세요
+            </div>
+        )
+    }
+}
